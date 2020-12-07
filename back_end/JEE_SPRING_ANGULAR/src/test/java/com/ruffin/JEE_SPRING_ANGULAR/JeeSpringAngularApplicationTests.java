4fbe96350bcd2f142fcd1fe1600b579ef0f7fd62
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-12-07 17:20:13 +0100

    add folders

---
+++ back_end/JEE_SPRING_ANGULAR/src/test/java/com/ruffin/JEE_SPRING_ANGULAR/JeeSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffin.JEE_SPRING_ANGULAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
